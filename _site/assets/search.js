16e7387659ddfb77f1d1a60302b4ea3f5267c529
zhangyuan948 <zhangyuan948@hellobike.com>
2021-08-12 16:10:06 +0800

    [add]设计模式——创建型模式

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2021/08/01/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"为什么要写blog？",'url':"/2021/08/01/welcome.html"},{'title':"设计模式——创建型模式",'url':"/2021/08/12/%E8%AE%BE%E8%AE%A1%E6%A8%A1%E5%BC%8F.html"}]};
